079356f092e67a80f22257dee3040c2891e2a1ad
sahariar8 <sahariaralam8@gmail.com>
2025-02-26 01:19:54 +0600

    Day 02 Task complete

---
+++ day-2/script.js
@@ -0,0 +1,41 @@
+// Task-1
+
+let name = "shahariar";
+let age = 29;
+let isStudent = true;
+let favouriteProgrammingLanguage = "Php,python & javascript"
+
+// Task-2
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favouriteProgrammingLanguage);
+
+// Task-3
+
+let fruit = "mango";
+console.log(fruit);
+fruit = "Coconut"
+console.log(fruit) // Coconut
+
+const fish = "Hilsha"
+console.log(fish);
+// fish = "pabda"
+// console.log(fish);// error
+
+// Task-4
+let obj1 = {name:"Jhon Doe"};
+let obj2 = obj1;
+obj2.name = "Jane Doe";//call by reference
+
+console.log(obj1);
+console.log(obj2);
+
+let num1 = 12;
+let num2 = num1;
+num2 = 55;
+
+console.log(num1);
+console.log(num2);
+
